b52a6549fec2ad2022e3559a843f9a15b4a79d97
islam7asan <59418425+islam7asan@users.noreply.github.com>
2020-04-10 02:22:13 +0200

    update search bar

--- js/first.js
+++ js/first.js
@@ -3,66 +3,60 @@ var productPriceInput = document.getElementById("productPriceInput");
 var productCategoryInput = document.getElementById("productCategoryInput");
 var productDescriptionInput = document.getElementById("productDescriptionInput");
 
-var productsContainer = JSON.parse ( localStorage.getItem("myProducts") ) ;
+var productsContainer = JSON.parse(localStorage.getItem("myProducts"));
 
-if(localStorage.getItem("myProducts") !=null)
-{
-    var productsContainer = JSON.parse ( localStorage.getItem("myProducts") ) ;
+if (localStorage.getItem("myProducts") != null) {
+    var productsContainer = JSON.parse(localStorage.getItem("myProducts"));
     displayProduct();
 }
-else
-{
+else {
     productsContainer = [];
 }
 
 
-function addProduct()
-{
-    var product = 
+function addProduct() {
+    var product =
     {
-        name:        productNameInput.value,
-        price:       productPriceInput.value,
-        category:    productCategoryInput.value,
+        name: productNameInput.value,
+        price: productPriceInput.value,
+        category: productCategoryInput.value,
         description: productDescriptionInput.value
     }
     productsContainer.push(product);
-    
-    localStorage.setItem("myProducts" , JSON.stringify(productsContainer) );
+
+    localStorage.setItem("myProducts", JSON.stringify(productsContainer));
 
 
     displayProduct();
     clearForm();
 }
 
-function displayProduct()
-{
-    var adder = "" ;
-    for(var i=0 ; i<productsContainer.length ; i++)
-    {
-        adder+=
-        `<tr>
-        <td>`+productsContainer[i].name+`</td>
-        <td>`+productsContainer[i].price+`</td>
-        <td>`+productsContainer[i].category+`</td>
-        <td>`+productsContainer[i].description+`</td>
-        <td>`+`<button onclick="updateProduct(`+i+`)" class="btn btn-warning">Update</button>`+`</td>
-        <td>`+`<button onclick="deleteProduct(`+i+`)" class="btn btn-danger">Delete</button>`+`</td>
+function displayProduct() {
+    var adder = "";
+    for (var i = 0; i < productsContainer.length; i++) {
+        adder +=
+            `<tr>
+        <td>`+ productsContainer[i].name + `</td>
+        <td>`+ productsContainer[i].price + `</td>
+        <td>`+ productsContainer[i].category + `</td>
+        <td>`+ productsContainer[i].description + `</td>
+        <td>`+ `<button onclick="updateProduct(` + i + `)" class="btn btn-warning">Update</button>` + `</td>
+        <td>`+ `<button onclick="deleteProduct(` + i + `)" class="btn btn-danger">Delete</button>` + `</td>
         </tr>`
     }
     document.getElementById("tableBody").innerHTML = adder;
 }
 
 
-function clearForm()
-{
-        productNameInput.value ="";
-        productPriceInput.value ="";
-        productCategoryInput.value ="";
-        productDescriptionInput.value ="";
+function clearForm() {
+    productNameInput.value = "";
+    productPriceInput.value = "";
+    productCategoryInput.value = "";
+    productDescriptionInput.value = "";
 }
 
 
-function searchProducts(term)
+/*function searchProducts(term)
 
 {
 
@@ -72,48 +66,60 @@ function searchProducts(term)
     {
         if(productsContainer[i].name.includes(term) == true )
         {
-            /*
+            
             var charIndex = productsContainer[i].name.indexof(term);
             var termLength = term.length;
             var part1= productsContainer[i].name.substring(0,charIndex);
             var part2= productsContainer[i].name.substring(charIndex,termLength);
             var element = `<p>` + part1 + `<span style="color:red">`+term+`</span>` + term + part2`</p>`;
             rowResults+=  element;
-            */
+            
 
             rowResults += `<p>`+productsContainer[i].name+`</p>`;
 
-            /*`<tr>
+            `<tr>
             <td>`+productsContainer[i].name+`</td>
             <td>`+productsContainer[i].price+`</td>
             <td>`+productsContainer[i].category+`</td>
             <td>`+productsContainer[i].description+`</td>
             </tr>`
-            */
+            
         }
 
         document.getElementById("rowResults").innerHTML = rowResults ;
     }
+}*/
+function searchProducts(term) {
+    var cont = "";
+    var cont2 = "";
+    var newTxt = '';
+    for (var i = 0; i < productsContainer.length; i++) {
+        if (productsContainer[i].name.includes(term.trim()) == true) {
+            cont += `<tr><td>` + productsContainer[i].name + `</td><td>` + productsContainer[i].price +
+                `</td><td>` + productsContainer[i].category + `</td><td>` + productsContainer[i].description + `</td><td><button onclick="updateProduct(` + i + `)" class="btn btn-warning">Update</button></td>
+                <td><button onclick="deleteProduct(` + i + `)" class="btn btn-danger">Delete</button></td>
+                </tr>`;
+            newTxt = productsContainer[i].name.replace(term, `<span class="font-weight-bold"  style="background-color: #eee;">` + term + `</span>`);
+            cont2 += `<p class="m-0 border p-1">` + newTxt + `</p>`;
+        }
+    }
+    if (term == "") {
+        cont2 = `<p></p>`;
+    }
+    document.getElementById("tableBody").innerHTML = cont;
+    document.getElementById("rowResults").innerHTML = cont2;
 }
 
 
-function deleteProduct(index)
-{
-   var answer = window.confirm("Are you sure you want to delete site") ;
-    if(answer)
-    {
-        productsContainer.splice(index,1);
-        localStorage.setItem("myProducts" , JSON.stringify(productsContainer) );
+function deleteProduct(index) {
+    var answer = window.confirm("Are you sure you want to delete site");
+    if (answer) {
+        productsContainer.splice(index, 1);
+        localStorage.setItem("myProducts", JSON.stringify(productsContainer));
         displayProduct();
     }
-    
 }
 
-function updateProduct(index)
-{
+function updateProduct(index) {
     window.alert(index);
 }
-
-
-
- 
